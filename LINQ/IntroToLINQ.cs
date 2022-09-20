eca822b619cd5bc96d0a995534d43250d0a9ab23
Giuliana Taylor <getaylor@sep.com>
2022-09-20 10:25:43 -0400

    Initial Commit

---
+++ LINQ/IntroToLINQ.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LINQ
+{
+    internal static class IntroToLinq
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Ohhi!");
+        }
+    }
+}
